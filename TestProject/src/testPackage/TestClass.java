07892fc64d338b5437e7f2179e7fbe9b6afdc1a2
VaibhavP555 <126932369+VaibhavP555@users.noreply.github.com>
2023-03-04 23:44:08 +0530

    Basic Task Commit

---
+++ TestProject/src/testPackage/TestClass.java
@@ -0,0 +1,12 @@
+package testPackage;
+
+public class TestClass {
+
+	public static void main (String args[]) {
+		
+		System.out.println("TestBase Class");
+		System.out.println("Util Class");
+		System.out.println("Pagelayer Class");
+		System.out.println("testlayer Class");
+	}
+}
